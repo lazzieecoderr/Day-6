6269bf1c76e8e92bdb61fc22cea9a6e75f96abca
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-03-27 11:54:42 +0530

    updated

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ class Movie {
      this.rating = rating;
   }
 }
- const obj1 = new Movie("Casino Royale","Eon Productions","PG13")
+ const obj = new Movie("leo","7-Screen Studio","7.2");
+ const obj1 = new Movie("vikram","RKFI","8.5")
  console.log(obj1.title,obj1.studio,obj1.rating);
 
  //!session practice task
  //!class person => name,age,gender,martial status,contact,email\
  //! class car => brand,color,model,type,price,section
- //! Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, 
- //!the studio “Eon Productions”, and the rating “PG­13”
+ //!Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, 
+ //the studio “Eon Productions”, and the rating “PG­13”
